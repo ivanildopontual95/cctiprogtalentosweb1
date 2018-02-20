01b7d73fae32808a66ed59f3bb0517c8dc081a52
Ivanildo <ivanildopontual95@gmail.com>
2018-02-20 12:00:13 -0400

    Inscrição Detalhes

--- resources/views/cartao.blade.php
+++ resources/views/cartao.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
     <div class="card-content">
-        <span class="card-title">{{$titulo}}</span>
+        <h5 class="header">{{$titulo}}</h5>
         <p>{{$descricao}}</p>
-        <p>Período de Inscrições: de {{$dataInicio}} - {{$horaInicio}} até {{$dataTermino}} - {{$horaTermino}} (Horário de Boa Vista).</p>
+        <p><strong>Período de Inscrições</strong> de {{$dataInicio}} - {{$horaInicio}}h até {{$dataTermino}} - {{$horaTermino}}h (Horário de Boa Vista).</p>
     </div>
     <div class="card-action">
         <a href="{{$url}}">Ver Mais</a>
--- resources/views/detalhes.blade.php
+++ resources/views/detalhes.blade.php
@@ -3,12 +3,35 @@
 
 <div class="container">
     <div class="row">
-        <div class="col s12 m12">
+        <div class="col s6">
             <div class="card">
                 <div class="card-stacked">
                     <div class="card-content">
                         <h5 class="header">{{$publicacao->titulo}}</h5>
                         <p>{{$publicacao->descricao}}</p>
+                        <p><strong>Cargos:</strong>{{$publicacao->cargos}}</p>
+                        <p><strong>Período de Inscrições:</strong> de {{$publicacao->dataInicio}} - {{$publicacao->horaInicio}}h até {{$publicacao->dataTermino}} - {{$publicacao->horaTermino}}h (Horário de Boa Vista).</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="!#">Inscreva-se!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col s6">
+            <div class="card">
+                <div class="card-stacked">
+                    <div class="card-content">
+                        <h5 class="header">Editais</h5>
+                        <div class="divider"></div>
+                        <div class="section">
+                        @foreach($publicacao->documentos as $documento)
+                            <p>{{ $documento->titulo }}</p>
+                            <div class="section">
+                            <a title="Baixar" class="btn green" href="{{ $documento->url }}" download><i class="material-icons">file_download</i></a>
+                            </div>
+                        @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
