17c3be6c6c76d5763f3a886df612b55163433ea7
Ivanildo Pontual Neto <32025067+ivanildopontual95@users.noreply.github.com>
2018-03-06 00:34:07 -0400

    Correção Login #2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,65 @@
 @extends('layouts.app')
-
 @section('content')
-
-<div class="container">
-    <div class="row">
-    </div>
-    <div class="row">
-        <div class="col s4 offset-s4">
-            <div class="card-panel white"> 
-                <center><img class="" src="/images/user.png"</i><center>
-                <p>Login</p>
-                <label>Insira suas Credenciais</label>
-                    @if (session('status'))
-                        <div class="card">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-      
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                    {{ csrf_field() }}
-                        <div class="input-field">
-                            <input type="text" name="email" value="{{ old('email') }}" class="validate" autofocus>
-                            <label>E-mail</label>
-                            @if ($errors->has('email'))
-                                <span>
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
+<div class="background-login">
+    <div class="container">
+        <div class="row">
+        </div>
+            <div class="row">
+                <div class="col s4 offset-s4">
+                    <div class="card-panel white"> 
+                        <center><img class="responsive-img" src="/images/user.png"</i><center>
+                        <h6>Bem-vindo!</h6>
+                        <label>Insira suas Credenciais</label>
+                            @if (session('status'))
+                                <div class="card">
+                                    {{ session('status') }}
+                                </div>
                             @endif
-                        </div>
-                        <div class="input-field">
-                            <input type="password"  name="password" value="{{ old('password') }}" class="validate">
-                            <label>Senha</label>
-                            @if ($errors->has('password'))
-                                <span>
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        <div class="row">
-                            <div class="checkbox">
-                                <input type="checkbox" id="remember" {{ old('remember') ? 'checked' : '' }}/>
-                                <label for="remember">Lembrar-me</label>
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                            {{ csrf_field() }}
+                                <div class="input-field">
+                                    <input type="text" name="email" value="{{ old('email') }}" class="validate" autofocus>
+                                    <label>E-mail</label>
+                                    @if ($errors->has('email'))
+                                        <span>
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="input-field">
+                                    <input type="password"  name="password" value="{{ old('password') }}" class="validate">
+                                    <label>Senha</label>
+                                    @if ($errors->has('password'))
+                                        <span>
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                                <div class="row">
+                                </div>
+                                    <div class="checkbox">
+                                        <input type="checkbox" class="filled-in" id="remember" {{ old('remember') ? 'checked' : '' }}/>
+                                        <label for="remember">Lembrar-me</label>
+                                    </div>
+                                <div class="row">
+                                </div>
+                                    <center><a href="{{ url('/password/reset') }}">Esqueceu sua Senha?</a><center>
+                        
+                                <div class="row">
+                                </div>
+                                    <button class="btn blue">Entrar</button>
+                                
+                            </form>
+                            <div class="row">
                             </div>
-                        </div>
-                        <div class="row">
-                            <center><a href="{{ url('/password/reset') }}">Esqueceu sua Senha?</a><center>
-                        </div>
-                        <div class="row">
-                            <button class="btn blue">Entrar</button>
-                        </div>
-                    </form>
-                    <div class="divider">ou</div>
-                    <div class="row">
+                            <div class="divider"></div>
+                            <div class="row">
+                            </div>
+                        <a title="Registrar" class="btn green" href="{{ url('/register') }}">Registre-se</a>
                     </div>
-                <a title="Registrar" class="btn green" href="{{ url('/register') }}">Registre-se</a>
+                </div>
             </div>
         </div>
     </div>
 </div>
-    
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,6 @@
         </script>
     </head>
     <body>
-        <div class="background-image"></div>
         @include('layouts._nave')
         <main>
             @yield('content')
@@ -37,6 +36,6 @@
                 document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.6'><\/script>".replace("HOST", location.hostname));
                 //]]>
             </script>
-@endif
+        @endif
     </body>
 </html>
