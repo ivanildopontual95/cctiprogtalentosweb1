ebde3866e8003216324378cfd417ac392df6ff57
Ivanildo <ivanildopontual95@gmail.com>
2018-03-23 15:09:26 -0400

    Menu Sandwich

--- public/js/app.js
+++ public/js/app.js
@@ -20904,9 +20904,6 @@ $(document).ready(function() {
   
   $('input.autocomplete').autocomplete({
     data: {
-      "Apple": null,
-      "Microsoft": null,
-      "Google": 'https://placehold.it/250x250'
     },
     limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
     onAutocomplete: function(val) {
@@ -20914,6 +20911,10 @@ $(document).ready(function() {
     },
     minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
   });
+  
+  $(document).ready(function () {
+    $(".button-collapse").sideNav();
+  })
 
 });
 
--- resources/assets/js/init.js
+++ resources/assets/js/init.js
@@ -100,9 +100,6 @@ $(document).ready(function() {
   
   $('input.autocomplete').autocomplete({
     data: {
-      "Apple": null,
-      "Microsoft": null,
-      "Google": 'https://placehold.it/250x250'
     },
     limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
     onAutocomplete: function(val) {
@@ -110,5 +107,9 @@ $(document).ready(function() {
     },
     minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
   });
+  
+  $(document).ready(function () {
+    $(".button-collapse").sideNav();
+  })
 
 });
