e9b944c599206473043d8930f32b986ac321d84e
ElytonSilva <31271721+ElytonSilva@users.noreply.github.com>
2018-03-23 12:39:41 -0400

    Login e Register Responsivo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,54 +6,59 @@
         <div class="row">
         </div>
         <div class="row">
-        <h5 class="left">Registro</h4>
         </div>
         <div class="row">
-            <div class="card-panel white">
-                <div class="row">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                        {{ csrf_field() }}
+            <div class="col s8 offset-s2">
+                <div class="card-panel white">
+                    <div class="row">
+                        <center><img class="responsive-img" src="/images/cctibulb.png"</i><center>
+                        <center><h6>Registre-se</h6></center>
+                        <center><label>Cadastre seus dados para acesso!</label></center>
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
+                            {{ csrf_field() }}
 
-                        <div class="input-field col s12">
-                            <input type="text" name="name" value="{{ old('name') }}" class="validate" autofocus>
-                            <label>Nome</label>
-                            @if ($errors->has('name'))
-                                <span>
-                                    <strong>{{ $errors->first('name') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        <div class="input-field col s12">
-                            <input type="text" name="email" value="{{ old('email') }}" class="validate">
-                            <label>E-mail</label>
-                            @if ($errors->has('email'))
-                                <span>
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        <div class="input-field col s12">
-                            <input type="password"  name="password" value="{{ old('password') }}" class="validate">
-                            <label>Senha</label>
-                            @if ($errors->has('password'))
-                                <span>
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        <div class="input-field col s12">
-                            <input type="password"  name="password_confirmation" value="{{ old('password_confirmation') }}" class="validate">
-                            <label>Confirme a senha</label>
-                            @if ($errors->has('password_confirmation'))
-                                <span>
-                                    <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                </span>
-                            @endif
-                        </div>
-                        <button class="btn green">Cadastrar</button>
-                    </form>
+                            <div class="input-field col s12">
+                                <input type="text" name="name" value="{{ old('name') }}" class="validate" autofocus>
+                                <label>Nome</label>
+                                @if ($errors->has('name'))
+                                    <span>
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="input-field col s12">
+                                <input type="text" name="email" value="{{ old('email') }}" class="validate">
+                                <label>E-mail</label>
+                                @if ($errors->has('email'))
+                                    <span>
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="input-field col s12">
+                                <input type="password"  name="password" value="{{ old('password') }}" class="validate">
+                                <label>Senha</label>
+                                @if ($errors->has('password'))
+                                    <span>
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="input-field col s12">
+                                <input type="password"  name="password_confirmation" value="{{ old('password_confirmation') }}" class="validate">
+                                <label>Confirme a senha</label>
+                                @if ($errors->has('password_confirmation'))
+                                    <span>
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="row">
+                            </div>
+                            <center><button class="btn green">Cadastrar</button></center>
+                        </form>
+                    </div>
                 </div>
-
             </div>
         </div>
     </div>
